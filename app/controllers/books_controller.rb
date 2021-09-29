f06d46a82025e80fc8c539545cd8fbee6dfa104b
sin0713 <narisin1124@gmail.com>
2021-09-29 04:17:27 +0000

    index_layout,create_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    
+    @book =Book.new
+    
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def new
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
+    
   end
 
   def edit
   end
 end
+
+
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+end
+
+
