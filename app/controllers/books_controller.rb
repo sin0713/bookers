c9cfb989d267df836258f00d7ba67b277aeb7f33
sin0713 <narisin1124@gmail.com>
2021-09-29 05:06:10 +0000

    [Add]show_layout, show_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,6 @@ class BooksController < ApplicationController
     
   end
 
-  def show
-    @book = Book.find(params[:id])
-  end
-
   def create
     book = Book.new(book_params)
     book.save
@@ -17,9 +13,23 @@ class BooksController < ApplicationController
     
   end
 
-  def edit
+  
+  def show
+    @book = Book.find(params[:id])
   end
-end
+
+ 
+  def edit
+    @book =Book.find(params[:id]) 
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+
 
 
 
