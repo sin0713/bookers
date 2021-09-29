385370297b7b047e80be477628dc8056b38610bd
sin0713 <narisin1124@gmail.com>
2021-09-29 05:58:06 +0000

    [Add]destroy_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,12 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book)
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
 
 
